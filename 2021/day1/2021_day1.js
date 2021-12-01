6954bc76fc6c87c8eab2a5d8654773433356e5b8
Addison Freeman <afreeman@qw-corp.com>
2021-12-01 17:01:30 -0600

    complete day 1 in python with reusable compare function for all list lookahead sizes

--- 2021/day1/2021_day1.js
+++ 2021/day1/2021_day1.js
@@ -1,8 +1,34 @@
+// let list = document.querySelector("body > pre").innerHTML.split("\n")
+// list = list.map((x, idx, arr) => { return parseInt(x) }).filter( x => !Number.isNaN(x) )
+// console.log(list)
+// let count = 0;
+// list.forEach((x, idx, arr) => { 
+//     if(idx === arr.length -1)
+//         return
+//     if (arr[idx+1] > x) {
+//         count++ 
+//     }
+//     console.log(`idx ${idx} is ${arr[idx+1]} > ${x} = ${count}`)
+// })
+// console.log(count)
+
+
 let list = document.querySelector("body > pre").innerHTML.split("\n")
 list = list.map((x, idx, arr) => { return parseInt(x) }).filter( x => !Number.isNaN(x) )
-console.log(list)
+
+tup = list.map((x, idx) => {
+    return [x, list[idx+1]]
+});
+
+trip = list.map((x, idx) => {
+    if(idx < list.length-2) 
+        return [x, list[idx+1], list[idx+2]]
+}).filter((x) => x !== undefined );
+
+let tripSum = trip.map(x => x[0] + x[1] + x[2]);
+
 let count = 0;
-list.forEach((x, idx, arr) => { 
+tripSum.forEach((x, idx, arr) => { 
     if(idx === arr.length -1)
         return
     if (arr[idx+1] > x) {
@@ -10,4 +36,3 @@ list.forEach((x, idx, arr) => {
     }
     console.log(`idx ${idx} is ${arr[idx+1]} > ${x} = ${count}`)
 })
-console.log(count)
--- 2021/day1/2021_day1.py
+++ 2021/day1/2021_day1.py
@@ -11,6 +11,8 @@ Advent of Code 2021 - Day 1
 Addison Freeman
 """
 
+# Part 1
+
 # grab list from file and splitby newline
 arr = open("./2021_day1.txt","r").read().splitlines()
 # ensure list<int>, call list() to execute map() on object
@@ -25,5 +27,28 @@ for i in enumerate(list_int):
     if (list_int[idx+1] > list_int[idx]):
          count += 1
 
-print(count)
+# print(count)
 
+# 
+# Part 2
+# 
+
+one = [ [x]  for i,x in enumerate(list_int[:-1]) ]
+tup = [ [x, list_int[i+1] ] for i,x in enumerate(list_int[:-1]) ]
+trip = [ [x, list_int[i+1], list_int[i+2] ] for i,x in enumerate(list_int[:-2]) ]
+quad = [ [x, list_int[i+1], list_int[i+2], list_int[i+3] ] for i,x in enumerate(list_int[:-3]) ]
+
+def compare(arr):
+    count = 0
+    for i in enumerate(arr):
+        idx = i[0]
+        if(idx > len(arr) - 2):
+            break
+        if (sum(arr[idx+1]) > sum(arr[idx])):
+            count += 1
+    print(count)
+
+compare(one)
+compare(tup)
+compare(trip)
+compare(quad)
