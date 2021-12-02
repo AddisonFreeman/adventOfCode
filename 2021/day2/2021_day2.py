f05d0b5dfdfd9c6c0edc29b9148183af6de24039
Addison Freeman <afreeman@qw-corp.com>
2021-12-02 01:28:32 -0600

    complete day 2, add aim to pos array and change instruction set

--- 2021/day2/2021_day2.py
+++ 2021/day2/2021_day2.py
@@ -7,21 +7,22 @@ Advent of Code 2021 - Day 2
 Addison Freeman
 """
 
-# Part 1
+# Part 1 & 2
 # grab list from file and splitby newline
 arr = open("./2021_day2.txt","r").read().splitlines()
 # split into command and value
 tup = [ [''.join([j for j in arr[i] if not j.isdigit()]).strip(), int(''.join(filter(str.isdigit, arr[i])))]  for i,x in enumerate(arr) ]
-
-pos = [0,0]
+# horizontal, vertical, aim
+pos = [0,0,0]
 def move(tup):
     for cmd,amt in tup:
         if(cmd == "forward"):
-            pos[0] += amt    
+            pos[0] += amt
+            pos[1] += pos[2] * amt
         elif(cmd == "up"):
-            pos[1] -= amt
+            pos[2] -= amt
         elif(cmd == "down"):
-            pos[1] += amt
+            pos[2] += amt
     return pos[0] * pos[1]
 
 res = move(tup)
